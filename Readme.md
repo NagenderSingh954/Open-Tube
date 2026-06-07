3e08c4e502f21f67de236d9c8308e621f8e02b09
Nagender Singh <nagendersingh954@gmail.com>
2026-06-08 02:52:50 +0530

    connect to mongo atlas

--- Readme.md
+++ Readme.md
@@ -1,1 +1,9 @@
 # This app was made in order to learn the Backend from the Chai our Code yt
+
+
+1st we have to make teh data base connection.there is various way to connectthe db we can write the connection copde in the index as we have tell teh node.js to execute from the index file mean it us the starting file of the program and another is that we can separate the code in different file this organize the code we will see teh both opproach
+
+
+2 Thing to remember about the database 
+   1) whenever we try to talk to alway wrap in the try catch becouse there may be the issue will arise and it is import that if the data not load then give the user response accordingly 
+   2) DB is on the another continenet so there is the time require for this always use Async Await 
--- src/index.js
+++ src/index.js
@@ -1,2 +1,46 @@
-console.log('hdf')
-console.log('hello')
+// require('dotenv').config({path:'./env'})      this also work but code consistency not match
+import dotenv from 'dotenv'                 //for consistency
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path:'./env'
+})
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// this is the 1st Approch to connect the DB
+
+/*import express from "express"
+const app=express()
+
+;(async ()=>{
+    try{
+       await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+       app.on("error",(error)=>{
+        console.log("App not able to talk: ",error)
+        throw error
+       })
+       app.listen(process.env.PORT,()=>{
+        console.log(`Application is listning on`,process.env.PORT)
+       })
+    }catch(err){
+        console.log("Error in DB Connection :", err)
+        throw err
+    }
+})()*/
